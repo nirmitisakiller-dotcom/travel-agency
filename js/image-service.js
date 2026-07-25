9a2045bf644761c0f34688de51a120094c4695b5
nirmitisakiller-dotcom <nirmitisakiller@gmail.com>
2026-07-25 20:54:27 +0530

    Update image-service.js

--- js/image-service.js
+++ js/image-service.js
@@ -4,6 +4,10 @@
 
 window.ImageService = {
 
+    // --------------------------------------
+    // Destination Images
+    // --------------------------------------
+
     destinations: {
 
         paris:
@@ -23,11 +27,68 @@ window.ImageService = {
 
     },
 
+    // --------------------------------------
+    // Attraction Images
+    // --------------------------------------
+
+    attractions: {
+
+        eiffel:
+            "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f",
+
+        louvre:
+            "https://images.unsplash.com/photo-1566139887280-c04d3f71d88f",
+
+        gardens:
+            "https://images.unsplash.com/photo-1525625293386-3f8f99389edd",
+
+        sentosa:
+            "https://images.unsplash.com/photo-1508964942454-1a56651d54ac",
+
+        ubud:
+            "https://images.unsplash.com/photo-1537953773345-d172ccf13cf1"
+
+    },
+
+    // --------------------------------------
+    // Hotel Images
+    // --------------------------------------
+
+    hotels: {
+
+        "marina-bay-sands":
+            "https://images.unsplash.com/photo-1566073771259-6a8506099945",
+
+        "pan-pacific":
+            "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa",
+
+        "bali-beach-resort":
+            "https://images.unsplash.com/photo-1571896349842-33c89424de2"
+
+    },
+
+    // --------------------------------------
+    // Functions
+    // --------------------------------------
+
     getDestinationImage(id) {
 
         return this.destinations[id] ||
+            "https://placehold.co/1200x700?text=Destination";
 
-            "https://placehold.co/1200x700?text=Nature+Tours";
+    },
+
+    getAttractionImage(id) {
+
+        return this.attractions[id] ||
+            "https://placehold.co/600x350?text=Attraction";
+
+    },
+
+    getHotelImage(id) {
+
+        return this.hotels[id] ||
+            "https://placehold.co/600x350?text=Hotel";
 
     }
 
