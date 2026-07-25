612c2056573d530c942c8adbf65718f6ce3bf0c9
nirmitisakiller-dotcom <nirmitisakiller@gmail.com>
2026-07-25 20:34:30 +0530

    Update image-service.js

--- js/image-service.js
+++ js/image-service.js
@@ -4,16 +4,31 @@
 
 window.ImageService = {
 
-    destination(name) {
-        return `https://source.unsplash.com/1200x700/?${encodeURIComponent(name)},travel`;
+    destinations: {
+
+        paris:
+            "https://images.unsplash.com/photo-1502602898657-3e91760cbb34",
+
+        singapore:
+            "https://images.unsplash.com/photo-1525625293386-3f8f99389edd",
+
+        bali:
+            "https://images.unsplash.com/photo-1537953773345-d172ccf13cf1",
+
+        tokyo:
+            "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf",
+
+        maldives:
+            "https://images.unsplash.com/photo-1573843981267-be1999ff37cd"
+
     },
 
-    hotel(name, city) {
-        return `https://source.unsplash.com/800x500/?${encodeURIComponent(name + " " + city + " hotel")}`;
-    },
+    getDestinationImage(id) {
 
-    attraction(name) {
-        return `https://source.unsplash.com/800x500/?${encodeURIComponent(name)}`;
+        return this.destinations[id] ||
+
+            "https://placehold.co/1200x700?text=Nature+Tours";
+
     }
 
 };
