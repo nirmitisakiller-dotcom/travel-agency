548b362cd4e204dcf7d0aa84e06be470b4a4f9f5
nirmitisakiller-dotcom <nirmitisakiller@gmail.com>
2026-07-31 12:11:16 +0530

    Refactor image retrieval functions for destinations and attractions

--- js/image-service.js
+++ js/image-service.js
@@ -8,33 +8,9 @@ window.ImageService = {
     // Destination Images
     // --------------------------------------
 
-    destinations: {
+    getDestinationImage(id) {
 
-        paris: "assets/destinations/paris.jpg",
-
-        tokyo: "assets/destinations/tokyo.jpg",
-
-        bali: "assets/destinations/bali.jpg",
-
-        singapore: "assets/destinations/singapore.jpg",
-
-        maldives: "assets/destinations/maldives.jpg",
-
-        leh: "assets/destinations/leh.jpg",
-
-        ladakh: "assets/destinations/ladakh.jpg",
-
-        spiti: "assets/destinations/spiti.jpg",
-
-        hampi: "assets/destinations/hampi.jpg",
-
-        chhattisgarh: "assets/destinations/chhattisgarh.jpg",
-
-        mumbai: "assets/destinations/mumbai.jpg",
-
-        nashik: "assets/destinations/nashik.jpg",
-
-        jalgaon: "assets/destinations/jalgaon.jpg"
+        return `assets/destinations/${id}.jpg`;
 
     },
 
@@ -42,17 +18,9 @@ window.ImageService = {
     // Attraction Images
     // --------------------------------------
 
-    attractions: {
+    getAttractionImage(id) {
 
-        eiffel: "assets/attractions/eiffel.jpg",
-
-        louvre: "assets/attractions/louvre.jpg",
-
-        gardens: "assets/attractions/gardens.jpg",
-
-        sentosa: "assets/attractions/sentosa.jpg",
-
-        ubud: "assets/attractions/ubud.jpg"
+        return `assets/attractions/${id}.jpg`;
 
     },
 
@@ -60,41 +28,9 @@ window.ImageService = {
     // Hotel Images
     // --------------------------------------
 
-    hotels: {
-
-        "marina-bay-sands":
-            "https://images.unsplash.com/photo-1566073771259-6a8506099945",
-
-        "pan-pacific":
-            "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa",
-
-        "bali-beach-resort":
-            "https://images.unsplash.com/photo-1571896349842-33c89424de2"
-
-    },
-
-    // --------------------------------------
-    // Functions
-    // --------------------------------------
-
-    getDestinationImage(id) {
-
-        return this.destinations[id] ||
-            "https://placehold.co/1200x700?text=Destination";
-
-    },
-
-    getAttractionImage(id) {
-
-        return this.attractions[id] ||
-            "https://placehold.co/600x350?text=Attraction";
-
-    },
-
     getHotelImage(id) {
 
-        return this.hotels[id] ||
-            "https://placehold.co/600x350?text=Hotel";
+        return `assets/hotels/${id}.jpg`;
 
     }
 
